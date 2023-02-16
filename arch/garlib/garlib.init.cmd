d158eccdafdc6d014032cc151d36f7d2f7a662e6
AyalaKaguya <ayalakaguya@outlook.com>
2023-02-16 20:34:05 +0800

    [bugfix] 空文件夹自动创建

--- arch/garlib/garlib.init.cmd
+++ arch/garlib/garlib.init.cmd
@@ -1,8 +1,9 @@
 @echo off
 set "PATH=%~dp0;%~dp0\runtime;%~dp0\buildin;%PATH%"
-set "_gfl.var=2.3"
+set "_gfl.var=2.4"
 set "_gfl.path=%~dp0"
 set "_gfl.power=Gardens Framework Library (GFL)"
+if not exist "%~dp0\runtime" mkdir "%~dp0\runtime"
 del /q "%~dp0\runtime\*"
 echo garlib:Already enabled
 echo %_gfl.power% v%_gfl.var%
