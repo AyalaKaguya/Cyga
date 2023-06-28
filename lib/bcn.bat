eb62f5f798c8491d54c773c8b4009340d15563b9
AyalaKaguya <ayalakaguya@outlook.com>
2023-06-28 15:49:22 +0800

    Update bcn.bat
    给TE某成员一点小小的全缩进震撼

--- lib/bcn.bat
+++ lib/bcn.bat
@@ -10,7 +10,6 @@ if exist "%Cyga_LibDir%\sbin\bcn.bat%" (
             if /i "%%k"=="/o" (
                 echo bcn:Error^(flag "/o" not allowed^!^)
                 goto :eof)))
-                
     setlocal
     set "/o=%Cyga_BinDir%"
 
@@ -19,15 +18,14 @@ if exist "%Cyga_LibDir%\sbin\bcn.bat%" (
         call "%~dp0\sbin\bcn.bat" get unrar)
     
     if /i not "%~2"=="bcn" (
-        call "%~dp0\sbin\bcn.bat" %*
-    ) else (
+        call "%~dp0\sbin\bcn.bat" %*) 
+    else (
         call "%~dp0\sbin\bcn.bat" %*
         move /y "%Cyga_BinDir%\bcn.bat" "%Cyga_LibDir%\sbin\bcn.bat" > nul
         del /q "%Cyga_BinDir%\bcn.rar"
         del /q "%Cyga_LibDir%\sbin\unrar.exe")
 
     endlocal
-    goto:eof
-
-) else (
+    goto:eof) 
+else (
     echo bcn:Error^(file not existed^!^))
